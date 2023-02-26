d88875fbfcd0523dcc4941afcbdccfe1ec09231b
muhammadkibria <muhammadmridulhosenkibria@gmail.com>
2023-02-26 14:45:08 +0600

    listView.builder and listView.separated done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+var names = ['Mridul', 'Aman Bhai', 'Emon Bhai', 'Towhid Bhai', 'Akash Bhai'];
+
 void main() {
   runApp(
     MaterialApp(
@@ -11,21 +13,17 @@ void main() {
         backgroundColor: Colors.amber,
         ),
 
-        body: ListView(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text("One", style: TextStyle(color: Colors.black, fontSize: 18),),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text("Two", style: TextStyle(color: Colors.black, fontSize: 18),),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text("Three", style: TextStyle(color: Colors.black, fontSize: 18),),
-            ),
-          ],
+        body: ListView.separated(itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(names[index], style: TextStyle(color: Colors.black, fontSize: 18, ),),
+          );
+        },
+        itemCount: names.length,
+        // itemExtent: 300,
+        separatorBuilder: (context, index) {
+          return Divider(height: 50, thickness: 2,);
+        },
         )
         
           
