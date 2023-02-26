1bdaff48aeefcac29e485d27e26dec59368c2225
muhammadkibria <muhammadmridulhosenkibria@gmail.com>
2023-02-26 11:40:02 +0600

    make any widget clickable using InkWell

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,47 +1,16 @@
 
 
-import 'package:flutter/material.dart';
-import 'package:google_nav_bar/google_nav_bar.dart';
-
-class HomePage extends StatefulWidget{
+void main(){
   
+  var listNames = [12,23,32,14,43,25];
+  var name = [];
+  name.add("hi there");
+  name.add(15);
+  name.add(25);
+  name.a dd(75);
 
+  name.addAll(listNames);
   
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage>{
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      
-      bottomNavigationBar: GNav(
-        activeColor: Color.fromARGB(255, 185, 185, 185),
-        backgroundColor: Color.fromARGB(255, 31, 31, 31),
-        color: Colors.white,
-       
-        tabBorder:  Border.all(
-          
-          color: Colors.white,width: 1.0,style: BorderStyle.solid),
-        tabs: [
-          GButton(icon: Icons.home,
-          text: 'Home',
-          ),
-          GButton(icon: Icons.favorite_border,
-          text: 'Likes',
-          ),
-          GButton(icon: Icons.search,
-          text: 'Search',
-          ),
-          GButton(icon: Icons.settings,
-          text: 'Settings',
-          ),
-        ]
-      
-      )
-      
-    );
-  }
+  
+  print(name);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,43 @@
 import 'package:flutter/material.dart';
-import 'homepage.dart';
 
-void main(){
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget{
-  const MyApp({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext){
-    return MaterialApp(
+void main() {
+  runApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
-  }
+      home: Scaffold(
+        appBar: AppBar(
+        title:const Text('First Flutter'),
+        ),
+
+        body: Container(
+          width: 200,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+            InkWell(
+              child: Text('Hi 1', style: TextStyle(fontSize: 24,),),
+              onTap: (){
+                print('tapped');
+              },
+              onDoubleTap: (){
+                print('double tapped');
+              },
+              onLongPress: (){
+                print('Long pressed');
+              },
+              ),
+            Text('Hi 2', style: TextStyle(fontSize: 24,),),
+            Text('Hi 3', style: TextStyle(fontSize: 24,),),
+            ElevatedButton(onPressed: (){
+              print("Clicked");
+            }, child: Text('Clicked'),),
+          ],
+          ),
+        )
+        
+            
+      ),
+   ),
+ );
 }
